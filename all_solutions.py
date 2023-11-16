b22b0e73df790adb22b1ccef187ef8a8e87b62a3
Gokberk Ozsoy <goezsoy@ethz.ch>
2023-11-16 21:50:50 +0000

    added sols 16 nov

--- all_solutions.py
+++ all_solutions.py
@@ -216,3 +216,237 @@ def searchRange(self, nums: List[int], target: int) -> List[int]:
         return [-1,-1]
     else:
         return [min(found), max(found)]
+    
+    
+# ---------------------------------------------------
+# 740. Delete and Earn
+# DP
+# time: O(n), space: O(n)
+
+# sol 1: bottom up (tabulation) + 2 variables
+def deleteAndEarn(self, nums: List[int]) -> int:
+    
+    scores = [0] * (max(nums)+1)
+
+    for num in nums:
+        scores[num] += num
+    
+    prev_prev = scores[0]
+    prev = scores[1]
+
+    for idx in range(2, len(scores)):
+
+        temp = prev
+        prev = max(prev, scores[idx]+prev_prev)
+        prev_prev = temp
+
+    return prev
+
+# sol 2: bottom up (tabulation) + memo
+def deleteAndEarn(self, nums: List[int]) -> int:
+    
+    scores = [0] * (max(nums)+1)
+
+    for num in nums:
+        scores[num] += num
+    
+    memo = [0] * len(scores)
+    memo[0] = scores[0]
+    memo[1] = scores[1]
+
+    for idx in range(2, len(scores)):
+
+        memo[idx] = max(memo[idx-1], scores[idx]+memo[idx-2])
+
+    return memo[-1]
+
+# sol 3: top down (memoization) + memo
+def deleteAndEarn(self, nums: List[int]) -> int:
+
+    scores = [0] * (max(nums)+1)
+
+    for num in nums:
+        scores[num] += num
+    
+    memo = [-1] * len(scores)
+
+    def aux(n):
+
+        if n==0 or n==1:
+            memo[n] = scores[n]
+            return memo[n]
+        else:
+            if memo[n]>=0:
+                return memo[n]
+            else:
+                result = max(scores[n]+aux(n-2), aux(n-1))
+                memo[n] = result
+                return memo[n]
+    
+    return aux(len(scores)-1)
+
+
+# ---------------------------------------------------
+# 5. Longest Palindromic Substring
+# DP
+# time: O(n^2), space: O(n^2)
+
+# sol 1: 1d solution. time limit exceeds. bottom-up (tabulation)
+def longestPalindrome(self, s: str) -> str:
+
+    def check(s):
+
+        for i in range(0, len(s)//2):
+
+            if s[i]!=s[len(s)-1-i]:
+                return False
+        
+        return True
+    
+    memo = {} # str: str's longest substr
+
+    def aux(s):
+    
+        if memo.get(s, None) is None:
+        
+            if check(s):
+                memo[s] = s
+                return memo.get(s)
+            else:
+                res1 = aux(s[1:])
+                res2 = aux(s[:-1])
+
+                if len(res1)>=len(res2):
+                    memo[s] = res1
+                else:
+                    memo[s] = res2
+                return memo.get(s)
+        
+        else:
+            return memo.get(s)
+
+    return aux(s)
+
+
+# sol 2: 2d solution. bottom-up (tabulation)
+def longestPalindrome(self, s: str) -> str:
+    
+    n = len(s)
+    curr_pal = s[0]
+
+    memo = [[False]*n for _ in range(n)]
+
+    for i in range(n):
+        memo[i][i] = True
+    
+    for i in range(n-1):
+
+        if s[i]==s[i+1]:
+            memo[i][i+1]=True
+            curr_pal = s[i:i+2]
+    
+    for sub_len in range(3, n+1):
+
+        for i in range(n-sub_len+1):
+
+            j = i + sub_len - 1
+
+            if memo[i+1][j-1] and s[i]==s[j]:
+                memo[i][j]=True
+                curr_pal = s[i:j+1]
+
+    return curr_pal
+
+
+# ---------------------------------------------------
+# 1143. Longest Common Subsequence
+# DP
+# time: O(n^2), space: O(n^2)
+
+# sol1: 2d solution.
+def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+    l1, l2 = len(text1), len(text2)
+
+    memo = [[0]*(l2+1) for _ in range(l1+1)]
+
+    
+    for i in range(1, l1+1):
+        for j in range(1, l2+1):
+
+            if text1[i-1]==text2[j-1]:
+                memo[i][j] = memo[i-1][j-1]+1
+            else:
+                memo[i][j] = max(memo[i-1][j], memo[i][j-1])
+    
+    return memo[-1][-1]
+
+# ---------------------------------------------------
+# 1143. 2140. Solving Questions With Brainpower
+# DP
+# time: O(n^2), space: O(n^2)
+
+# sol1: plain recursion. time limit exceeds
+def mostPoints(self, questions: List[List[int]]) -> int:
+
+    def aux(curr, idx):
+    
+        if len(questions)<=idx:
+            return curr
+        else:
+            score, jump = questions[idx]
+            
+            return max(aux(curr+score,idx+jump+1), aux(curr, idx+1))
+            
+    return aux(0, 0)
+
+# sol2: ?? TODO
+
+
+# ---------------------------------------------------
+#1980. Find Unique Binary String
+# backtracking
+# time: O(2^n), space: O(2^n)
+
+# sol 1: 
+def findDifferentBinaryString(self, nums: List[str]) -> str:
+
+    from collections import Counter
+
+    c = Counter(nums)
+    n = len(nums)
+
+    def aux(curr_str):
+
+        if len(curr_str)==n:
+            if not c.get(curr_str):
+                return [curr_str]
+            else:
+                return []
+        else:
+            return aux(curr_str+'0') + aux(curr_str+'1')
+
+    return aux('')[0]
+
+# sol 2:
+def findDifferentBinaryString(self, nums: List[str]) -> str:
+
+    from collections import Counter
+
+    c = Counter(nums)
+    n = len(nums)
+
+    def aux(curr_str):
+
+        if len(curr_str)==n:
+            if not c.get(curr_str):
+                return curr_str
+        else:
+            
+            res1 = aux(curr_str+'0')
+            if res1:
+                return res1
+
+            return aux(curr_str+'1')
+
+    return aux('')
