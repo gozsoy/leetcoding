5fa7b52250719e0fb2b2e8b8a20da4fa48828fce
Gokberk Ozsoy <goezsoy@ethz.ch>
2024-03-09 23:32:37 +0000

    added 3 js sols

--- all_solutions.js
+++ all_solutions.js
@@ -27,7 +27,6 @@ var flat = function (arr, n) {
 // ---------------------------------------------------
 // 2705. Compact Object
 // recursion
-// time: ?, space: ?
 
 var compactObject = function(obj) {
 
@@ -154,3 +153,178 @@ class EventEmitter {
  * sub.unsubscribe(); // undefined
  * emitter.emit('onClick'); // []
  */
+
+
+// ---------------------------------------------------
+// 2721. Execute Asynchronous Functions in Parallel
+
+var promiseAll = function(functions) {
+
+    return new Promise((resolve, reject) =>{
+
+        let arr = Array(functions.length).fill(0)
+        let resolved_cnt = 0
+
+        functions.forEach(async (el, idx) => {
+    
+            try {
+                const res = await el()
+                arr[idx] = res
+                resolved_cnt++
+                
+                if (resolved_cnt == functions.length){
+                    resolve(arr)
+                }
+            }
+            catch(err) {
+                reject(err)
+            }
+
+        })
+
+    })
+    
+};
+
+// ---------------------------------------------------
+// 2636. Promise Pool
+
+var promisePool = async function(functions, n) {
+
+    return new Promise((resolve) => {
+
+        let pending_cnt = 0
+        let curr_idx = 0
+
+        function aux(){
+            
+            if (curr_idx >= functions.length){
+                if (pending_cnt == 0){
+                    resolve()
+                }
+            }
+
+            while (pending_cnt < n && curr_idx < functions.length){
+                
+                pending_cnt += 1
+                const promise = functions[curr_idx]()
+                curr_idx += 1
+
+                promise.then(() =>{
+                    pending_cnt-=1
+                    aux()
+                })
+            }
+
+        }
+        
+        aux()
+    })
+
+};
+
+// ---------------------------------------------------
+// 2632. Curry
+
+var curry = function(fn) {
+    
+    let arg_sofar = []
+    let total_args = fn.length
+
+    return function curried(...args) {
+        
+        arg_sofar.push(...args)
+
+        if (total_args == arg_sofar.length){
+            return fn(...arg_sofar)
+        }
+        else{
+            return curried
+        }
+
+    }
+};
+
+
+// ---------------------------------------------------
+// 2628. JSON Deep Equal
+
+var areDeeplyEqual = function(o1, o2) {
+    
+    // primitive
+    if (Object(o1)!==o1){
+        if (Object(o2)!==o2 && o1===o2){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    // array
+    else if (Array.isArray(o1)){
+        if (Array.isArray(o2) && o1.length==o2.length){
+
+            for (let idx=0; idx<o1.length;idx++){
+                if(!areDeeplyEqual(o1[idx], o2[idx])){
+                    return false
+                }
+            }
+
+            return true
+
+        }
+        else{
+            return false
+        }
+
+    }
+    // object
+    else{
+        if (!Array.isArray(o2) && Object(o2)===o2  && Object.keys(o1).sort().toString()==Object.keys(o2).sort().toString()){
+            
+            for (const temp_key in o1){
+                if (!areDeeplyEqual(o1[temp_key], o2[temp_key])){
+                    return false
+                }
+            }
+
+            return true          
+        }
+        else{
+            return false
+        }
+
+    }
+
+};
+
+// ---------------------------------------------------
+// 2676. Throttle
+
+var throttle = function(fn, t) {
+    
+    let latest_args = null;
+    let is_working = null;
+
+    const aux = () => {
+        if (latest_args==null){
+            is_working=null
+        }
+        else{
+            fn(...latest_args)
+            latest_args=null
+            is_working=setTimeout(aux, t)
+        }
+    }
+
+    return function(...args) {
+        
+        if (!is_working){
+            fn(...args)
+            is_working = setTimeout(aux, t)
+        }
+        else{
+            latest_args = args
+        }
+    }
+};
